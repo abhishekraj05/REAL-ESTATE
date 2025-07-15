7f9ba0112db44f1e89529bea23ab2bf91ca3dc52
abhishekraj05 <abhishekraj995554@gmail.com>
2025-07-15 14:17:04 +0530

    first commit ❤

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Footer from "./components/Footer";
@@ -8,27 +7,15 @@ import AboutPage from "./components/About/AboutPage";
 import PropertyPage from "./components/Property/PropertyPage";
 import NavContactPage from "./components/NavContact/NavContactPage";
 
-// ✅ Inline ScrollToTop function inside App file
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
-
 function App() {
   return (
     <HashRouter>
-      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/properties" element={<PropertyPage />} />
-        <Route path="/contact" element={<NavContactPage />} />
+        <Route path="/AboutPage" element={<AboutPage />} />
+        <Route path="/PropertyPage" element={<PropertyPage />} />
+        <Route path="/NavContactPage" element={<NavContactPage />} />
       </Routes>
       <Footer />
     </HashRouter>
